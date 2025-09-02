0db0957a880c95daa79ee1442ce457a76aac4ee9
Vinay Chauhan <136556320+Vihan2754@users.noreply.github.com>
2025-09-02 15:10:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"https://ai-assistant-frontend-one.vercel.app",
+    origin:"http://localhost:5173",
     credentials:true
 }))
 const port=process.env.PORT || 5000
